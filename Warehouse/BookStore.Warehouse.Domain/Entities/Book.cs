a77df9dffafdcc1e948320c0e3c09d33fa276733
Murilo Beltrame <murilobeltrame@gmail.com>
2021-06-16 01:56:47 -0300

    4th commit

---
+++ Warehouse/BookStore.Warehouse.Domain/Entities/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.Warehouse.Domain.Entities
+{
+    public record Book
+    {
+        public Book(
+            string title,
+            IEnumerable<Author> authors,
+            Publisher publisher,
+            ushort? pages = null
+        ) => (Title, Authors, Publisher, Pages) = (title, authors, publisher, pages);
+
+        private IEnumerable<Author> _authors;
+        public IEnumerable<Author> Authors
+        {
+            get => _authors;
+            init => _authors = (value?.Any()).GetValueOrDefault() ? value : throw new ArgumentException("", nameof(Authors));
+        }
+
+        private Publisher _publisher;
+        public Publisher Publisher
+        {
+            get => _publisher;
+            init => _publisher = value ?? throw new ArgumentException("", nameof(Publisher));
+        }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            init => _title = !string.IsNullOrWhiteSpace(value) ? value : throw new ArgumentNullException(nameof(Title));
+        }
+
+        public ushort? Pages { get; }
+    }
+}
---
+++ Warehouse/BookStore.Warehouse.Domain/Entities/WarehouseItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BookStore.Warehouse.Domain.Entities
+{
+    public class WarehouseItem
+    {
+        public WarehouseItem(Book book, int? quantity) => (Book, Quantity) = (book, quantity);
+
+        private Book _book;
+        public Book Book
+        {
+            get => _book;
+            init => _book = value != null ? value : throw new ArgumentNullException("", nameof(Book));
+        }
+        public int? Quantity { get; }
+    }
+}
